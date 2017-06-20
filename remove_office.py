dfc79ded4cd6fbb86a23afa2200dbf74505abdd5
Myles Rayfield <myles.rayfield@icloud.com>
2017-06-19 23:16:31 -0400

    Saving Work

--- remove_office.py
+++ remove_office.py
@@ -5,3 +5,12 @@
 
 import sys
 import os
+
+applications = '/Applications'
+ms_office = '/Applications/Microsoft\Office'
+ms_link = 'Applications/Microsoft Link.app'
+
+for app in os.listdir(applications):
+    #os.remove('Microsoft Office 2011')
+    os.remove('Docker.app')
+    print(app)
